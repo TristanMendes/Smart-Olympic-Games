f4e682b4d3a2e02ea64f3b3da4e11f14b76c4732
SoussiMohamedNour <soussi.mohamednour@gmail.com>
2021-10-25 20:02:07 +0200

    Factures Source File

---
+++ factures.cpp
@@ -0,0 +1,18 @@
+#include "factures.h"
+
+factures::factures()
+{
+    reference = "";
+    nbService = 0;
+    statut = 0;
+    montant = 0;
+
+    dateCreation.jour = 0;
+    dateCreation.mois = 0;
+    dateCreation.annee = 0;
+    dateCreation.jour = 0;
+    dateCreation.mois = 0;
+    dateCreation.annee= 0;
+
+
+}
