6fbb600f802fb8fc8a0c36c07349299dbd551dac
SoussiMohamedNour <soussi.mohamednour@gmail.com>
2021-10-25 19:59:45 +0200

    DB Connection Source File

---
+++ connection.cpp
@@ -0,0 +1,20 @@
+#include "connection.h"
+
+connection::connection()
+{
+
+}
+
+bool connection::createConnect()
+{
+    bool test = false;
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setDatabaseName("projet");
+    db.setUserName("soussi");
+    db.setPassword("soussi");
+
+    if(db.open())
+        test = true;
+
+   return test;
+}
