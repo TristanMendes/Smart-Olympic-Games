5ba19216fa3f1be97968e58dcc18ee1e6c16bf39
mariem <mariem.marsaoui@esprit.tn>
2021-11-22 21:54:49 +0100

    mise à jour smtp.h

--- smtp.h
+++ smtp.h
@@ -1,3 +1,14 @@
+/*
+Copyright (c) 2013 Raivis Strogonovs
+
+http://morf.lv
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
 #ifndef SMTP_H
 #define SMTP_H
 
@@ -11,8 +22,7 @@
 #include <QByteArray>
 #include <QFile>
 #include <QFileInfo>
-#include <QtNetwork>
-#include <QSslSocket>
+
 
 
 class Smtp : public QObject
@@ -21,10 +31,13 @@ class Smtp : public QObject
 
 
 public:
-    Smtp( QString user, QString pass, const QString &host, int port = 465, int timeout = 30000 );
+    Smtp( const QString &user, const QString &pass,
+          const QString &host, int port = 465, int timeout = 30000 );
     ~Smtp();
 
-    void sendMail( QString from, QString to, QString subject, QString body);
+    void sendMail( const QString &from, const QString &to,
+                   const QString &subject, const QString &body,
+                   QStringList files = QStringList());
 
 signals:
     void status( const QString &);
