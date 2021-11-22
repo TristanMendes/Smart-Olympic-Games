d2f2c73b08d8a4d1c9fddb0f5f6045e62ce3e822
mariem <mariem.marsaoui@esprit.tn>
2021-11-22 21:54:31 +0100

    mise à jour smtp.cpp

--- smtp.cpp
+++ smtp.cpp
@@ -1,6 +1,19 @@
+/*
+Copyright (c) 2013 Raivis Strogonovs
+
+http://morf.lv
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.*/
+
+
+
 #include "smtp.h"
 
-Smtp::Smtp( QString user, QString pass, const QString &host, int port, int timeout )
+Smtp::Smtp( const QString &user, const QString &pass, const QString &host, int port, int timeout )
 {    
     socket = new QSslSocket(this);
 
@@ -21,15 +34,56 @@ Smtp::Smtp( QString user, QString pass, const QString &host, int port, int timeo
 
 }
 
-void Smtp::sendMail(QString from, QString to,  QString subject, QString body)
+void Smtp::sendMail(const QString &from, const QString &to, const QString &subject, const QString &body, QStringList files)
 {
     message = "To: " + to + "\n";    
     message.append("From: " + from + "\n");
     message.append("Subject: " + subject + "\n");
+
+    //Let's intitiate multipart MIME with cutting boundary "frontier"
+    message.append("MIME-Version: 1.0\n");
+    message.append("Content-Type: multipart/mixed; boundary=frontier\n\n");
+
+
+
+    message.append( "--frontier\n" );
+    //message.append( "Content-Type: text/html\n\n" );  //Uncomment this for HTML formating, coment the line below
+    message.append( "Content-Type: text/plain\n\n" );
     message.append(body);
+    message.append("\n\n");
+
+    if(!files.isEmpty())
+    {
+        qDebug() << "Files to be sent: " << files.size();
+        foreach(QString filePath, files)
+        {
+            QFile file(filePath);
+            if(file.exists())
+            {
+                if (!file.open(QIODevice::ReadOnly))
+                {
+                    qDebug("Couldn't open the file");
+                    QMessageBox::warning( 0, tr( "Qt Simple SMTP client" ), tr( "Couldn't open the file\n\n" )  );
+                        return ;
+                }
+                QByteArray bytes = file.readAll();
+                message.append( "--frontier\n" );
+                message.append( "Content-Type: application/octet-stream\nContent-Disposition: attachment; filename="+ QFileInfo(file.fileName()).fileName() +";\nContent-Transfer-Encoding: base64\n\n" );
+                message.append(bytes.toBase64());
+                message.append("\n");
+            }
+        }
+    }
+    else
+        qDebug() << "No attachments found";
+
+
+    message.append( "--frontier--\n" );
+
     message.replace( QString::fromLatin1( "\n" ), QString::fromLatin1( "\r\n" ) );
-    message.replace( QString::fromLatin1( "\r\n.\r\n" ),
-    QString::fromLatin1( "\r\n..\r\n" ) );
+    message.replace( QString::fromLatin1( "\r\n.\r\n" ),QString::fromLatin1( "\r\n..\r\n" ) );
+
+
     this->from = from;
     rcpt = to;
     state = Init;
@@ -39,9 +93,10 @@ void Smtp::sendMail(QString from, QString to,  QString subject, QString body)
      }
 
     t = new QTextStream( socket );
+
+
+
 }
-
-
 
 Smtp::~Smtp()
 {
